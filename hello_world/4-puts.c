64fe8500fb8dde635f36e35603a21cd22baf922b
hmeyd <yyahmedsalm@gmail.com>
2024-10-21 23:30:20 +0200

    modif

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 int main()
 {	 
-    system("ltrace -bc ./4-puts_0");
+   
     int num = puts("Programming is like building a multilingual puzzle");
     printf("\n%d", num);
        

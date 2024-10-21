6e7ad4a83efcbbac936bc4df34024d66397e56fd
hmeyd <yyahmedsalm@gmail.com>
2024-10-21 23:47:47 +0200

    modif

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main()
 {	 
    
-    int num = puts("Programming is like building a multilingual puzzle");
+    puts("Programming is like building a multilingual puzzle");
     printf("\n%d", num);
        
     return 0;

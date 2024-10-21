066db0759923111d2789c3eee8ce0f49e7b2ddc4
hmeyd <yyahmedsalm@gmail.com>
2024-10-22 00:51:13 +0200

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
-/*ecrire un commentaire*/
 #include <stdio.h>
 int main(void)
 {
+// Ceci est un commentaire sur une seule ligne
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

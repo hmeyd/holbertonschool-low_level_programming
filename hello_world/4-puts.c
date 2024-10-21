75796556e39a5fa48fe7efd3c95b92db6170e3cc
hmeyd <yyahmedsalm@gmail.com>
2024-10-22 00:15:02 +0200

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    puts("programming is like building a multilingual puzzle,\n");
+    puts("\"programming is like building a multilingual puzzle");
     return 0;
 }

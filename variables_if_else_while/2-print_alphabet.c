d42d7ada43bab6811c9313504b61a0334bb798df
hmeyd <yyahmedsalm@gmail.com>
2024-10-22 15:48:15 +0200

    print alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-int main() {
-    char c;
-    for (c = 'a'; c <= 'z'; ++c)
-    {
-        putchar(c);
-    }
-    putchar('\n');
-    return 0;
+/**
+*main -Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }

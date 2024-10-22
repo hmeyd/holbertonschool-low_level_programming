87a70c2f0903b48a8de22560ed711063fe080f59
hmeyd <yyahmedsalm@gmail.com>
2024-10-22 22:55:24 +0200

    base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char i;
+for (i = 0; i <= 15; ++i)
+{
+if (i<11)
+putchar(i + '0');
+else 
+putchar(i - 10 + 'a')
+}
+putchar('\n');
+return (0);
+}

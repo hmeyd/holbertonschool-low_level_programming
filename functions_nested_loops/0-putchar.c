eb5337d27de0f711476370b980024612544fc871
hmeyd <yyahmedsalm@gmail.com>
2024-10-23 11:48:11 +0200

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,7 +7,14 @@
 
 int	main(void)
 {
-	_putchar("ahmedsal");
-	putchar('\n');
+	_putchar('h');
+	_putchar('H');
+    	_putchar('e');
+	_putchar('l');
+	_putchar('l');
+	_putchar('o');
+	_putchar('l');
+        _putchar('o');
+	_putchar('\n');
 	return (0);
 }

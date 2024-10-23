06c1bfea9d8af78d09205d2ec983915e9737cce0
hmeyd <yyahmedsalm@gmail.com>
2024-10-23 15:34:07 +0200

    isolower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*islower -Entry point
+*Return: void
+*/
+int _islower(int c)
+{
+	if (c <= 'a' && c <= 'z')
+		_putchar('1');
+	else
+		_putchar('0');
+	_putchar('\n');
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 int	_putchar();
 void	print_alphabet();
 void	print_alphabet_x10();
+int	_islower();
 #endif

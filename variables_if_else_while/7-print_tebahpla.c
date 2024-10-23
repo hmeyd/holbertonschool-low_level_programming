e695cb2272204499b8646d364b99b99f01c4a5c3
hmeyd <yyahmedsalm@gmail.com>
2024-10-23 09:22:48 +0200

    modif

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 char i;
-for (i = 'z'; i >= 'a'; --i)
+for (i = 'z'; i Écrivez un programme qui imprime toutes les combinaisons possibles de nombres à un chiffre.
 {
 putchar(i);
 }

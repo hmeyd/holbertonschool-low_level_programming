66744367a936f57d4e67bf4ccca2adbdec2af2e4
hmeyd <yyahmedsalm@gmail.com>
2024-10-23 16:26:44 +0200

    modif

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
-*islower -Entry point
-*Return: void
-*/
+ * _islower - Vérifie si un caractère est en minuscule
+ * @c: Le caractère à vérifier
+ *
+ * Return: 1 si le caractère est une lettre minuscule, 0 sinon
+ */
 int _islower(int c)
 {
 	if ('a' <= c && c <= 'z')
-		return(1);
+		return (1);
 	else
 		return (0);
 }

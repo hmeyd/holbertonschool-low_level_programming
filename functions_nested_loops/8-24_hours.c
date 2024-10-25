ce981efc8e5853641041869e9e4eabfae4832a68
hmeyd <yyahmedsalm@gmail.com>
2024-10-25 16:12:45 +0200

    modif

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,28 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * print_last_digit - print the laste digit
- * @n: the number
- *
- * Return:Returns the value of the last digit
+ * jack_bauer - prints every minute of the day
  */
 void jack_bauer(void)
 {
 
-int i , j;
+int i, j;
 
-        for (i = 0;i <= 23; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		
-		for (j = 0;j <= 59; j++)
+		for (j = 0; j <= 59; j++)
 		{
-                		_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-                		_putchar(':');
-				_putchar(( j / 10) + '0');
-				_putchar((j % 10) + '0');
-				_putchar('\n');
-
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }

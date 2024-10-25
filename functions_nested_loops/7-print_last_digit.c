ecd9f5dc6f81ee3ca0c28f6bbeb67248fb0c24eb
hmeyd <yyahmedsalm@gmail.com>
2024-10-25 14:19:16 +0200

    modif

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,20 @@
  */
 int print_last_digit(int n)
 {
-
-	int a;
-
-	a = abs(n) % 10;
-	_putchar(a + '0');
-	return (a);
+int p;
+	if (n > 0)
+	{
+	p = n % 10;
+	}
+	else if (n < 0)
+	{
+	n = -n;
+	p = n % 10;
+	}
+	else
+	{	
+	p = 0;
+	}
+	_putchar(p + '0');
+	return (p);
 }

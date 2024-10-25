ad8ba175d437eb9e1281f504ce64ad1e55722838
hmeyd <yyahmedsalm@gmail.com>
2024-10-25 11:26:10 +0200

    modif

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,29 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _abs - camculer le valeur abs
- * @n: Le nombre
+ * print_last_digit - print the laste digit
+ * @n: the number
  *
- * Return:retun la valeur absoulue du nombre et 0 s'il est nulle
+ * Return:Returns the value of the last digit
  */
 int print_last_digit(int n)
 {
 
-	int a; int p;
-	if (n < -10 || n > 10)
-	{
-	p = abs(n);
-	a = p % 10;
-	return (a);
-	}
-	else if (-10 < n || n < 10)
-	{
-	a = abs(n);
-        return (a);
-	}
-	else
-	{
-	return(n);
-	}
+	int a;
 
+	a = abs(n) % 10;
+	_putchar(a + '0');
+	return (a);
 }

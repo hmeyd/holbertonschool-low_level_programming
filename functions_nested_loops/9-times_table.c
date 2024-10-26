1540a87a496635f3a7085ee413a3166b152311df
hmeyd <yyahmedsalm@gmail.com>
2024-10-26 17:07:43 +0200

    modif

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,11 +1,35 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * times_table - function multiplication table
  *
- * Return: Always 0.
+ * @x, r, c : variable resultat, raw, column of the multiplication's table
+ *
+ * Return: void return (nothing)
  */
+
 void times_table(void)
 {
-    times_table();
-    return (0);
+	int i, j, x;
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+		x = j * i;
+		if (x < 9)
+		{
+		_putchar(i + '0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		}
+		else
+		{ 
+		_putchar((x % 10) + '0');
+		_putchar((x / 10) + '0');
+                _putchar(',');
+                _putchar(' ');
+		}
+	}
+}
+

44205187f707fbcf6d0e5cfd06e47eea1697b1df
hmeyd <yyahmedsalm@gmail.com>
2024-10-27 21:05:48 +0100

    modif

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,52 +7,50 @@
  */
 void print_to_98(int n)
 {
-	int i, k, l;
+	int i;
 
 if (n > 98)
 {
 	for (i = n; i >= 98; i--)
 	{
-		if (i > 100)
-		{
-			k = i / 100;
-			l = k * 100;
+			if (i > 100)
+			{
 			_putchar((i / 100) + '0');
-			_putchar(((i / 100) % 10) + '0');
+			_putchar(((i / 10) % 10) + '0');
 			_putchar((i % 10) + '0');
-			_putchar('\n');
-		}
-		else
-		{
+			}
+			else
+			{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
-			_putchar('\n');
-		}
+			}
 	}
 }
 else
 {
 	for (i = n; i <= 98; i++)
 	{
-		if (i < 10)
-		{
-			if (i < 0)
-			{
-				_putchar('-');
-			}
-			_putchar(i + '0');
-			_putchar('\n');
-		}
-		else
-		{
-			if (i < 0)
-			{
-				_putchar('-');
-			}
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar('\n');
-		}
+	if (i < -100)
+	{
+	_putchar((i / 100) + '0');
+	_putchar(((i / 100) % 10) + '0');
+	_putchar((i % 10) + '0');
+	}
+	else if (i < -10 && i > -99)
+	{
+	_putchar((i / 10) + '0');
+	_putchar((i % 10) + '0');
+	}
+	else if (i < 10 && i > (-10))
+	{
+	_putchar(i + '0');
+	}
+	else if (i < 98 && i > 10)
+	{
+	_putchar((i / 10) + '0');
+	_putchar((i % 10) + '0');
 	}
 }
 }
+}
+

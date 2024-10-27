8f33b1e4a13c90c6e8d4d3df4f5336a357bc06ea
hmeyd <yyahmedsalm@gmail.com>
2024-10-27 21:17:19 +0100

    modif

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,11 +9,6 @@ void print_to_98(int n)
 {
 	int i;
 
-if(n == 98)
-{
-_putchar('9');
-_putchar('8');
-}
 else if (n > 98)
 {
 	for (i = n; i >= 98; i--)
@@ -57,5 +52,6 @@ else
 	}
 }
 }
+-putchar('\n');
 }
 

da5649941f562fa338a5b628d86ab3c470d0685e
hmeyd <yyahmedsalm@gmail.com>
2024-10-28 17:37:27 +0100

    reset to

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *reset_to_98 - points the parametre to 98
+ *
+ * @n: parameter that will be pointed
+ * void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+#include <stdlib.h>
+int     _putchar();
+void	reset_to_98(int *n);
+#endif

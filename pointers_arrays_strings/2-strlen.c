a674fe919b8ce4fb18e482d129205f75fc9da279
hmeyd <yyahmedsalm@gmail.com>
2024-10-28 18:16:07 +0100

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - calculer la longeur d'un mot
+ *
+ * @s: length pointer
+ * Return: la longeur de string
+ */
+int _strlen(char *s)
+{
+char a;
+a = strlen(s);
+return (a);
+}

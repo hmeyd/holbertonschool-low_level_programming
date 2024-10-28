dc804e423328087960f3ec0f0e7658a75e5c7e15
hmeyd <yyahmedsalm@gmail.com>
2024-10-28 01:49:31 +0100

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * main - check the code.
+ * _isupper - reconait le majuscule et le minuscule
+ *@c: majuscule ou miniscule
  *
- * Return: Always 0.
+ * Return: return 1 if c is upper and return 0 if is not
  */
 int _isupper(int c)
 {
 
 if (c <= 'Z' && c >= 'A')
 return (1);
-else 
+else
 return (0);
 }
 

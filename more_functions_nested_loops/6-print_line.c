b68139ae21afe756e2306bbc4d332609bcfa7574
hmeyd <yyahmedsalm@gmail.com>
2024-10-28 10:11:17 +0100

    print line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,16 @@ void print_line(int n)
 {
 int i;
 
-for (i = 0; i <= n; i++)
-{
 if (n > 0)
 {
+for (i = 1; i <= n; i++)
+{
 _putchar('_');
+}
 }
 else
 {
 _putchar('\n');
-}
 }
 _putchar('\n');
 }

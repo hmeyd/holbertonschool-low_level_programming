e05c330682856c5f88c42ca3cf07da486e2bb730
hmeyd <yyahmedsalm@gmail.com>
2024-10-28 02:06:28 +0100

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+int mul;
+
+mul = a *b;
+return (mul);
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,4 +4,5 @@
 int     _putchar();
 int 	_isupper(int c);
 int	_isdigit(int c);
+int	mul(int a, int b);
 #endif

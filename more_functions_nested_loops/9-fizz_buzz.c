cc6d514c24309c57be22d9e4a2ba7355696eb878
hmeyd <yyahmedsalm@gmail.com>
2024-10-28 13:43:21 +0100

    fizz buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+int main ()
+{
+int i;
+
+for (i =0; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d",i);
+}
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}

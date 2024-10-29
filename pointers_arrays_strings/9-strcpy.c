309e42532f6c34bd6cf3a36964c950a5575968af
hmeyd <yyahmedsalm@gmail.com>
2024-10-29 21:21:58 +0100

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -5,18 +5,19 @@
  *
  * @dest: char le chain de charactere
  * @src: l'element
- * Return: 
+ * Return: la copie du chaine de caractere
  */
 char *_strcpy(char *dest, char *src)
 {
-int i = 0, j = 0;
+int i = 0, j;
+
 while (src[i] != '\0')
 {
 i++;
 }
 for (j = 0; j <= (i + 1); j++)
 {
-dest[j] = src[i];
+dest[j] = src[j];
 }
 return (dest);
 }

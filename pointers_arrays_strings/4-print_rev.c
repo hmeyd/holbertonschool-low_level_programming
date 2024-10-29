76e3d564dcda51fb1cb2a149237b500525ee5e0b
hmeyd <yyahmedsalm@gmail.com>
2024-10-29 01:04:17 +0100

    print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ *
+ * @s: le sting à afficher en sens inverse
+ * void
+ */
+void print_rev(char *s)
+{
+int i = 0, a = 0;
+while (s[i] != '\0')
+{
+i = i + 1;
+}
+while ((i - a) != 0)
+{
+_putchar(s[i - a]);
+a = a + 1;
+}
+_putchar('\n');
+}

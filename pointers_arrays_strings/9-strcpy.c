ca07aa8c5eb0e34ad3066c13416dc0e07230ca36
hmeyd <yyahmedsalm@gmail.com>
2024-10-29 17:50:21 +0100

    modif

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - prints n elements
+ *
+ * @dest: char le chain de charactere
+ * @src: l'element
+ * Return: 
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0, j = 0;
+while (src[i] != '\0')
+{
+i++;
+}
+for (j = 0; j <= (i + 1); j++)
+{
+dest[j] = src[i];
+}
+return (0);
+}

b7ece4413d62ee3091363dbf45010aee74e0ad99
hmeyd <yyahmedsalm@gmail.com>
2024-10-29 14:30:13 +0100

    rev string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - prints a string in reverse
+ *
+ * @s: le sting à afficher en sens inverse
+ * void
+ */
+void rev_string(char *s)
+{
+char sit;
+int i = 0, len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+while (i < len / 2)
+{
+sit = s[len - i -1];
+s[len - i -1] = s[i];
+s[i] = sit;
+i++;
+}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
 #ifndef main_h
 #define main_h
 #include <stdlib.h>
-int     _putchar();
+int     _putchar(char c);
 void	reset_to_98(int *n);
 void	swap_int(int *a, int *b);
 int 	_strlen(char *s);
 void	_puts(char *str);
 void	print_rev(char *s);
+void	rev_string(char *s);
 #endif

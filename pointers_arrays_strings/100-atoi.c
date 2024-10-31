7841eb220774056e9a2f9f2530bb43f5555c2731
hmeyd <yyahmedsalm@gmail.com>
2024-10-31 01:48:32 +0100

    modif

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,12 +6,17 @@
  * @s: char le chain de charactere
  * Return: la copie du chaine de caractere
  */
+int _atoi(char *s)
+{
+int i = 0, si = 1, re = 0, f = 0;
+
 while (s[i] != '\0')
 {
-if ((s[i] == '-' && (s[i + 1] >= '0' && s[i + 1] <= '9')) || s[i + 1] == '-')
+if (s[i] == '-' || s[i] == '+')
+{
+if (s[i] == '-' && (s[i + 1] == '-' || s[i + 1] == '+' || (s[i + 1] >= '0' && s[i + 1] <= '9')))
 si *= -1;
-else if (s[i] == '+' && f != 1)
-;
+}
 else if (s[i] >= '0' && s[i] <= '9')
 {
 f = 1;

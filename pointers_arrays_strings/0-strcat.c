f44355a87622ecb6cf350029f49a7af1414ca2aa
hmeyd <yyahmedsalm@gmail.com>
2024-11-01 00:27:34 +0100

    modif

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcat - reconait les entier
+ * @dest:la phrase a fusionner 
+ * @stc: à utiliser
+ * Return: fusionner deux ²chaine de caractere
+ */
+char *_strcat (char *dest, char *src)
+{
+int i = 0, j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}

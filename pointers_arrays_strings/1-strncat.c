121781f6c337bd57b25dab3c881a6c068ac906ae
hmeyd <yyahmedsalm@gmail.com>
2024-11-01 14:37:15 +0100

    modif

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -4,11 +4,17 @@
  * @dest:la phrase a fusionner
  * @src: à utiliser n bite
  * n: le nombre d'octet à utiliser
- * Return: fusionner deux ²chaine de caractere
+ * Return: fusionner deux chaine de caractere
  */
 char *_strncat(char *dest, char *src, int n)
 {
 int i = 0, j = 0;
+if (dest[0] == '\0' && src[0] == '\0')
+{
+return (dest);
+}
+else
+{
 while (dest[i] != '\0')
 {
 i++;
@@ -20,4 +26,5 @@ i++;
 }
 dest[i] = '\0';
 return (dest);
+}
 }

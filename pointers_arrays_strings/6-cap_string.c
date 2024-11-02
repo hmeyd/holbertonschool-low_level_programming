c39ff8b424a779b1bb620440b4d3ca3ba6c06877
hmeyd <yyahmedsalm@gmail.com>
2024-11-02 08:14:21 +0100

    modif

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,9 +10,12 @@ char *cap_string(char *str)
 int i = 0;
 while (str[i] != '\0')
 {
-if (str[i] == ' ')
+if (str[i] == ' '))
+{
+if (str[i + 1] <= 'z' && str[i + 1] >= 'a')
 {
 str[i + 1] = str[i + 1] - 32;
+}
 }
 i++;
 }

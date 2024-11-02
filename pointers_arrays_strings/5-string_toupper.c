1118f4734d5d553503efbae5c97ba9147a233637
hmeyd <yyahmedsalm@gmail.com>
2024-11-02 07:52:17 +0100

    modif

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -3,15 +3,14 @@
  * string_toupper - prints a string in reverse
  *
  * @str: l'array à afficher en sens inverse
- * 
- * Return: le string
+ * Return:le string
  */
 char *string_toupper(char *str)
 {
 int i = 0;
 while (str[i] != '\0')
 {
-if (str[i] <='z' && str[i] >= 'a')
+if (str[i] <= 'z' && str[i] >= 'a')
 {
 	str[i] = str[i] - 32;
 }

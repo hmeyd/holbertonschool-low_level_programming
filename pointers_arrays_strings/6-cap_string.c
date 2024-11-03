3e2316641a5c3985a3be99195e265db5e17f4806
hmeyd <yyahmedsalm@gmail.com>
2024-11-03 10:07:43 +0100

    modif

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,6 +7,7 @@
  */
 char *cap_string(char *str)
 {
+char s = " \t\n,;.!?\"(){}"
 int i = 0;
 while (str[i] != '\0')
 {
@@ -14,11 +15,14 @@ if (str[0] <= 'z' && str[0] >= 'a')
 {
 str[0] = str[0] - 32;
 }
-if (str[i] == ' ' || str[i] == '	' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] =='.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] ==')' || str[i] =='{' || str[i] == '}')
+for (j = 0; j <= 12)
+{
+if (s[j] == str[i])
 {
 if (str[i + 1] <= 'z' && str[i + 1] >= 'a')
 {
 str[i + 1] = str[i + 1] - 32;
+}
 }
 }
 i++;

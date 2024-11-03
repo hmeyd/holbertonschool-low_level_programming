3341625de2bd2c9e952c294811c883f858feaffc
hmeyd <yyahmedsalm@gmail.com>
2024-11-03 10:44:45 +0100

    modif

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,21 +7,20 @@
  */
 char *leet(char *str)
 {
-    int i, j;
-    char letters[] = "aAeEoOtTlL";
-    char leet[] = "43071";
+int i, j;
+char letters[] = "aAeEoOtTlL";
+char leet[] = "43071";
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; letters[j] != '\0'; j++)
-        {
-            if (str[i] == letters[j])
-            {
-                str[i] = leet[j / 2];
-                break;
-            }
-        }
-    }
-
-    return str;
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; letters[j] != '\0'; j++)
+{
+if (str[i] == letters[j])
+{
+str[i] = leet[j / 2];
+break;
+}
+}
+}
+return str;
 }

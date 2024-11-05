3c8a58d72566cbbb03dd6ba171a1efa2457ac267
hmeyd <yyahmedsalm@gmail.com>
2024-11-06 00:05:41 +0100

    modif

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -3,20 +3,16 @@
 /**
  * print_diagsums - locates a character in a string
  * @a: the matrix
- * size: dimenssion
+ * @size: dimenssion
  *
  * Void
  */
-void print_diagsums(int *a,int size)
+void print_diagsums(int *a, int size)
 {
-int i, j, sum1 = 0, sum2 = 0;
+int i, sum1 = 0, sum2 = 0;
 for (i = 0; i < size; i++)
 {
-        for (j = 0; j < size; j++)
-        {
-		sum1 += a[size * i + i];
-		sum2 += a[size * i + (size - i - 1)];
-        }
+	sum1 += a[size * i + i];
+	sum2 += a[size * i + (size - i - 1)];
 }
-printf("%d, %d\n",sum1,sum2);
 }

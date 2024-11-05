79a14a8cdd3cfd28136088ea64014b4e8e1ddf98
hmeyd <yyahmedsalm@gmail.com>
2024-11-05 23:56:32 +0100

    modif

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,18 +9,14 @@
  */
 void print_diagsums(int *a,int size)
 {
-int i, j;
-
-for (i = 0; i <= size; i++)
+int i, j, sum1 = 0, sum2 = 0;
+for (i = 0; i < size; i++)
 {
-        for (j = 0; j <= size; j++)
+        for (j = 0; j < size; j++)
         {
-                if (i == j)
-		sum1 = sum1 + a[i][j];
-		else if (j == size - i)
-		sum2 = sum2 +  a[i][j];
+		sum1 += a[size * i + i];
+		sum2 += a[size * i + (size - i - 1)];
         }
 }
-_putchar(sum1);
-_putchar(sum2);
+printf("%d, %d",sum1,sum2);
 }

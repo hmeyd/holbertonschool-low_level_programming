48048cbe409d0352926b13c39dd3ea82b1920916
hmeyd <yyahmedsalm@gmail.com>
2024-11-05 23:13:47 +0100

    modif

--- pointers_arrays_strings/7-print_chessboard2.c
+++ pointers_arrays_strings/7-print_chessboard2.c
@@ -10,12 +10,24 @@
 void print_chessboard(char (*a)[8])
 {
 int i, j;
+char *b = "rkbqkbkr";
 for (i = 0; i <= 7; i++)
 {
         for (j = 0; j <= 7; j++)
         {
-                _putchar(a[i][j]);
-        }
+                if (i == 0)
+                        a[i][j] = b[j];
+		else if (i == 1)
+			a[i][j] = 'p';
+		else if (i == 6)
+			a[i][j] = 'P';
+		else if (i == 7)
+			a[i][j] = b[j] -32;
+		else
+			a[i][j] = ' ';
+		_putchar(a[i][j]);
+	}
 	_putchar('\n');
 }
 }
+

c4abae84827dda1af6f0d041d067cab5fd7b9e05
hmeyd <yyahmedsalm@gmail.com>
2024-11-06 02:03:15 +0100

    modif

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/* _puts_recursion - print a string per recursion
+ * @s: the string
+ * Void
+ *
+ * */
+void _puts_recursion(char *s)
+{
+
+_putchar(*s);
+s++;
+if (*s)
+{
+	_puts_recursion(s);
+}
+_putchar('\n');
+}

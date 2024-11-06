38a79b00a793740b66f1dbaf5f80fd14415bf89d
hmeyd <yyahmedsalm@gmail.com>
2024-11-07 00:42:43 +0100

    modif

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,25 +4,25 @@
  * sqrt_helper - Fonction auxiliaire pour trouver la racine carrée naturelle
  * @n: Le nombre dont on veut trouver la racine carrée
  * @guess: La tentative actuelle pour la racine carrée
- * Return: La racine carrée naturelle de n, ou -1 si n n'a pas de racine carrée entière
+ * Return: La racine carrée naturelle de n, ou -1 sinon
  */
 int sqrt_helper(int n, int guess)
 {
-    if (guess * guess == n)
-        return guess;
-    if (guess * guess > n)
-        return -1;
-    return sqrt_helper(n, guess + 1);
+if (guess * guess == n)
+	return (guess);
+if (guess * guess > n)
+	return (-1);
+return (sqrt_helper(n, guess + 1));
 }
 
 /**
  * _sqrt_recursion - Renvoie la racine carrée naturelle d'un nombre
  * @n: Le nombre dont on veut trouver la racine carrée
- * Return: La racine carrée naturelle de n, ou -1 si n n'a pas de racine carrée entière
+ * Return: La racine carrée naturelle de n, ou -1 sinon
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
-    return sqrt_helper(n, 0);
+if (n < 0)
+	return (-1);
+return (sqrt_helper(n, 0));
 }

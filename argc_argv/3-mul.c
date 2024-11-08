4e5ed3c3bba07e9ce16e9adc90412a03141e2bc1
hmeyd <yyahmedsalm@gmail.com>
2024-11-08 02:01:38 +0100

    modif

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Affiche le nom du programme
+ * @argc: Nombre d'arguments de la ligne de commande
+ * @argv: Tableau des arguments de la ligne de commande
+ *
+ * Return: Toujours 0 (succès)
+ */
+int main(int argc, char *argv[])
+{
+int a, b;
+if (argc > 3)
+{
+printf("Error\n");
+return (1);
+}
+else 
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a * b);
+}
+return (0);
+}

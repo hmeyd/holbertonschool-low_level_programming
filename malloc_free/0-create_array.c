d8cac5505df922de059ffc06d98800974ce0d5d0
hmeyd <yyahmedsalm@gmail.com>
2024-11-11 22:44:45 +0100

    modif

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,7 +18,8 @@ if (size == 0)
 arr = malloc(size * sizeof(char));
 if (arr == NULL)
 {
-	return (NULL);
+	printf("Échec de l'allocation de mémoire\n");
+        return (1);
 }
 for (i = 0; i < size; i++)
 {

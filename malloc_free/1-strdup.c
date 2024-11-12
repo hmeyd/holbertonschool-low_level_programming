cf9be58b4a7ffdc4ca1e2f251ddef93781058025
hmeyd <yyahmedsalm@gmail.com>
2024-11-12 11:52:41 +0100

    modif

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -8,22 +8,26 @@
  */
 char *_strdup(char *str)
 {
-int size = 0, i;
-char *arr;
-if (str == NULL)
-{
-	return (NULL);
-}
-while (str[size] != '\0')
-{
-	size = size + 1;
-}
-arr = (char *)malloc((size) * sizeof(char) + 1);
-for (i = 0; i < size; i++)
-{
-	arr[i] = str[i];
-}
-arr[size] = '\0';
-return (arr);
+	int size = 0, i;
+	char *arr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[size] != '\0')
+	{
+		size = size + 1;
+	}
+	arr = malloc((size) * sizeof(char) + 1);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = str[i];
+	}
+	arr[size + 1] = '\0';
+	return (arr);
 }
 

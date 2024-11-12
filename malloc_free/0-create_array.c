1d0859845afb3932c7aed45eed2f065824ecfbee
hmeyd <yyahmedsalm@gmail.com>
2024-11-12 11:28:20 +0100

    modif

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,20 +9,21 @@
  */
 char *create_array(unsigned int size, char c)
 {
-unsigned int i;
-char *arr;
-if (size == 0)
-{
-	return (NULL);
-}
-arr = (char *) malloc(size * sizeof(char));
-if (arr == NULL)
-{
-        return (NULL);
-}
-for (i = 0; i < size; i++)
-{
-	arr[i] = c;
-}
-return (arr);
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(size);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
 }

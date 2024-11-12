7c16ed746c557ed21e09c39dd726d7210c661342
hmeyd <yyahmedsalm@gmail.com>
2024-11-13 00:35:37 +0100

    modif

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	}
 	arr = malloc(height * sizeof(int *));
 	if (arr == NULL)
-		return (0);
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));

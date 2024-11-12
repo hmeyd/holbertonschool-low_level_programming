dd9cff352fe3894f844514bc78583a55f5970406
hmeyd <yyahmedsalm@gmail.com>
2024-11-12 22:48:42 +0100

    modif

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,10 +12,11 @@ char *str_concat(char *s1, char *s2)
 	int size = 0, size1 = 0, i, j;
 	char *arr;
 
-	if (s1 == NULL || s2 == NULL)
+	if (*s1 == '\0' && *s2 == '\0')
 	{
-		return (NULL);
+		return ('\0');
 	}
+
 	while (s1[size] != '\0')
 	{
 		size = size + 1;
@@ -24,16 +25,23 @@ char *str_concat(char *s1, char *s2)
 	{
 		size1 = size1 + 1;
 	}
-	arr = (char *)malloc((size + size1) * sizeof(char) + 1);
+	arr = (char *)malloc((size + size1 + 1) * sizeof(char));
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	else if (size == 0)
+		arr = s2;
+	else if (size1 == 0)
+		arr = s1;
+	else 
 	{
-		arr[i] = s1[i];
-	}
-	for (j = 0; j <= size1; j++)
-	{
-		arr[j + size] = s2[j];
+		for (i = 0; i < size; i++)
+		{
+			arr[i] = s1[i];
+		}
+		for (j = 0; j <= size1; j++)
+		{
+			arr[j + size] = s2[j];
+		}
 	}
 	arr[size  + size1 + 1] = '\0';
 	return (arr);

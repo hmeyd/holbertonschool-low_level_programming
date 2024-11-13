f022450c01c03886948dbde752654bf1efe87657
hmeyd <yyahmedsalm@gmail.com>
2024-11-13 02:44:33 +0100

    modif

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -7,15 +7,16 @@
  * malloc_checked - checker les alloucation
  * @b: la dimenssion
  *
- * Void
+ * Return: arr
  */
 void *malloc_checked(unsigned int b)
 {
 	int *arr;
+
 	arr = malloc(b * sizeof(int));
 	if (arr == NULL)
 	{
-		exit (98);
+		exit(98);
 	}
 	return (arr);
-}   
+}

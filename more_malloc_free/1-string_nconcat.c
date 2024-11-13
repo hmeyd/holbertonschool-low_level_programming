3a2a75943703814eecf79e740fb47e33358cec55
hmeyd <yyahmedsalm@gmail.com>
2024-11-13 03:37:38 +0100

    modif

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -42,7 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			arr[i] = s1[i];
 		for (j = 0; j < n; j++)
 			arr[i + j] = s2[j];
-		arr[i + j + 1] = '\0';
+		arr[i + j] = '\0';
 	}
 	return (arr);
 }

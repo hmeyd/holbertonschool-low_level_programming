75a2ff741f609ace086f6eb41cec79057762cac3
hmeyd <yyahmedsalm@gmail.com>
2024-11-13 01:06:44 +0100

    modif

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -10,8 +10,13 @@
 
 void free_grid(int **grid, int height)
 {
+	int i;
 	if (grid != NULL && height != 0)
 	{
-	free(grid);
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
 	}
+	free(grid);
 }

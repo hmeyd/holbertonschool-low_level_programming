47f8d7b954ec8701fae0e96aeffcd60c95f7c8e8
hmeyd <yyahmedsalm@gmail.com>
2024-11-15 04:15:18 +0100

    modif

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -21,11 +21,7 @@ void print_dog(struct dog *d)
 	if (name == NULL)
 		printf("Name: (nil)");
 	age = d->age;
-	if (age == 0)
-		 printf("Age: (nil)");
 	owner = d->owner;
-	if(owner == NULL)
-		 printf("Owner: (nil)");
 	}
 	printf("Name: %s\nAge: %.1f\nOwner: %s\n", name, age, owner);
 }

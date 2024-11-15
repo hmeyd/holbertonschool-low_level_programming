d447e6df46053194656ce7c9f1aa3ccb6545f331
hmeyd <yyahmedsalm@gmail.com>
2024-11-15 03:03:06 +0100

    modif

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - Initialise une structure dog avec des valeurs spécifiques
+ * @d: Pointeur vers une structure de type dog à initialiser
+ * @name: Nom du chien
+ * @age: Âge du chien
+ * @owner: Nom du propriétaire du chien
+ *
+ * Description: Cette fonction initialise les membres de la structure dog
+ * pointée par d avec les valeurs spécifiées pour name, age et owner.
+ * Elle ne fait rien si le pointeur d est NULL.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

abb98396977c7e39366a5525a1302163e42788d7
hmeyd <yyahmedsalm@gmail.com>
2024-11-15 04:33:39 +0100

    modif

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -5,7 +5,7 @@
  * print_dog - affiche les information du rechercher
  * @d: Pointeur vers une structure de type dog à initialiser
  * Description: Cette fonction initialise les membres de la structure dog
- * pointée par d avec les valeurs spécifiées pour name, age et owner.
+ * pointée par d avec les valeurs spécifiées pour name, age et owner
  * Elle ne fait rien si le pointeur d est NULL.
  */
 void print_dog(struct dog *d)
@@ -13,14 +13,23 @@ void print_dog(struct dog *d)
 	char *name, *owner;
 	float age;
 
+	if (d == NULL)
+	{
+		return;
+	}
 	name = d->name;
 	if (name == NULL)
 		printf("Name: (nil)");
+	else
+		printf("Name: %s\n", name);
 	age = d->age;
 	if (age == 0)
 		printf("Age: (nil)");
+	else
+		printf("Age: %f\n", age);
 	owner = d->owner;
 	if (owner == NULL)
 		printf("Owner: (nil)");
-	printf("Name: %s\nAge: %.1f\nOwner: %s\n", name, age, owner);
+	else
+		printf("Owner: %s\n", owner);
 }

a75180b7383c97c8764ccae535585cfe9beb0d9b
hmeyd <yyahmedsalm@gmail.com>
2024-11-16 01:20:41 +0100

    modif

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,6 +19,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (0);
+	if (name == NULL)
+		return;
+	if (owner == NULL)
+		return;
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;

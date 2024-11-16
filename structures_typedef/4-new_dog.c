d6f359a95aa541bcdaa71051ffcf81121fad23fe
hmeyd <yyahmedsalm@gmail.com>
2024-11-16 02:33:00 +0100

    modif

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 #include <string.h>
 /**
  * new_dog - affiche les information du rechercher
@@ -8,37 +9,56 @@
  * @owner: le nouvau properietaire
  * Return: la nouvelle structure
  * Description: Cette fonction initialise les membres de la structure dog
+ * pointée par d avec les valeurs spécifiées poiur name, age et owner
+ * Elle ne fait rien si le pointeur d est NULL.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	char *new_name, *new_owner;
+	int i, ol = 0, nl = 0;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-		return (NULL);
+		return(NULL);
 	if (name != NULL)
 	{
-		new_name = malloc((strlen(name) + 1) * sizeof(char));
+		while (name[nl] != '\0')
+		{
+			nl++;
+		}
+		new_name = malloc((nl + 1) * sizeof(char));
 		if (new_name == NULL)
 		{
 			free(new_dog);
 			return (NULL);
 		}
-		strcpy(new_name, name);
+		for (i = 0; name[i]; i++)
+		{
+			new_name[i] = name[i];
+		}
+		new_name[i] = '\0';
 	}
 	else
 		new_name = NULL;
 	if (owner != NULL)
 	{
-		new_owner = malloc((strlen(owner) + 1) * sizeof(char));
+		while (owner[ol] != '\0')
+		{
+			ol++;
+		}
+		new_owner = malloc((ol + 1) * sizeof(char));
 		if (new_owner == NULL)
 		{
 			free(new_dog);
 			free(new_name);
 			return (NULL);
 		}
-		strcpy(new_owner, owner);
+		for (i = 0; owner[i]; i++)
+		{
+			new_owner[i] = owner[i];
+		}
+		new_owner[i] = '\1';
 	}
 	else
 		new_owner = NULL;

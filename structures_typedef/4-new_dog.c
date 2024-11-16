1593d11aa34bc630bba85d81202a87a90f217f09
hmeyd <yyahmedsalm@gmail.com>
2024-11-16 02:16:12 +0100

    modif

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * new_dog - affiche les information du rechercher
  * @name: Pointeur vers une structure de type dog à initialiser
@@ -16,34 +16,52 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	char *new_name, *new_owner;
-	int i, nl = 0, ol = 0;
+	int i, ol = 0, nl = 0;
+
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-		return(0);
+		return(NULL);
+	if (name != NULL)
+	{
 	while (name[nl] != '\0')
 	{
 		nl++;
 	}
-	new_name = malloc(nl * (sizeof(char)) + 1);
-	for (i = 0; i < nl; i++)
+	new_name = malloc((nl + 1) * sizeof(char));
+	if (new_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
 	{
 		new_name[i] = name[i];
 	}
-	new_name[nl] = '\0';
-	if (owner == NULL)
-		return(0);
-	while (owner[ol] != '\0')
-	{
-		ol++;
+	new_name[i] = '\0';
 	}
-	new_owner = malloc(ol * (sizeof(char)) + 1);
+	else
+		new_name = NULL;
+	if (owner != NULL)
+	{
+		while (owner[ol] != '\0')
+		{
+			ol++;
+		}
+	new_owner = malloc((ol + 1) * sizeof(char));
 	if (new_owner == NULL)
-		return (0);
-	for (i = 0; i <= ol; i++)
+	{
+		free(new_dog);
+		free(new_name);
+		return (NULL);
+	}
+	for (i = 0; owner[i]; i++)
 	{
 		new_owner[i] = owner[i];
 	}
-	new_owner[ol] = '\0';
+	new_owner[i] = '\0';
+	}
+	else 
+		new_owner = NULL;
 	new_dog->name = new_name;
 	new_dog->age = age;
 	new_dog->owner = new_owner;

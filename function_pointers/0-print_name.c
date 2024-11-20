fc7cf8a6870cae6c62eaf23fe7f8dc8cdc963aa1
hmeyd <yyahmedsalm@gmail.com>
2024-11-20 03:26:52 +0100

    modif

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name as is
  * @name: name of the person
- *
+ * @f: une fonction pointeur
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
 	void (**p)(char *);
+
 	p = &f;
 	(*p)(name);
 }

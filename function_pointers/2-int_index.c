cdb44743354b1d761ab2697a14dd651f8f40527f
hmeyd <yyahmedsalm@gmail.com>
2024-11-20 04:44:48 +0100

    modif

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -2,19 +2,24 @@
 #include "function_pointers.h"
 
 /**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
- *
+ * int_index - check index
+ * @array:the integer to check
+ * @size: size de l'array
+ * @cmp: comperateur des entiers
  * Return: 0 if false, something else otherwise.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL || size == NULL)
+	{
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
 	}
-return (0);
+	return (0);
 }

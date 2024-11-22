2a65b35ee53ba41250d48e3828d828fa4bf2600d
hmeyd <yyahmedsalm@gmail.com>
2024-11-22 15:12:59 +0100

    modif

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 
 /**
- * print_numbers - Calcule la somme de tous les arguments entiers donnés
+ * print_strings - Calcule la somme de tous les arguments entiers donnés
  * @n: Le nombre d'entiers passés à la fonction
  * @separator: c'est un separateur entre le argument
- * Return: La somme de tous les arguments, ou 0 si n est 0
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	char* str;
+	char *str;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)

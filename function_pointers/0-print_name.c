6e0fe47b3e7f6e50a644263acf5f0683ced25b6f
hmeyd <yyahmedsalm@gmail.com>
2024-11-25 12:22:57 +0100

    modif

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Imprime un nom en utilisant une fonction de rappel
- * @name: Nom de la personne à imprimer
- * @f: Pointeur vers une fonction qui prend une chaîne en
- * Return: Rien.
+ * print_name - prints a name
+ * @name: name to print
+ * @f: fuction that prints a mane
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)

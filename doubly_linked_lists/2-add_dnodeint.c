a2124096266157250a664e3e530a7d606cae0078
hmeyd <yyahmedsalm@gmail.com>
2024-12-05 13:09:48 +0100

    modif

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,15 +17,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		return (NULL);
 	}
+	new_node->n = n;
 	if (*head)
 	{
-		new_node->n = n;
 		new_node->prev = NULL;
 		new_node->next = *head;
+		(*head)->prev = new_node;
 		*head = new_node;
 		return (new_node);
 	}
-	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
 	*head = new_node;

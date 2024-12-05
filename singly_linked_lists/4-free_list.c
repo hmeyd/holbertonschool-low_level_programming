9cc76b151980a8c84523bbb43b0ad586b3c07685
hmeyd <yyahmedsalm@gmail.com>
2024-12-05 09:10:39 +0100

    modif

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,13 +7,14 @@
  */
 void free_list(list_t *head)
 {
-    list_t *temp;
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp->str);
-        free(temp);
-    }
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
 

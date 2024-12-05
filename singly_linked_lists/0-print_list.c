94c5c53d6bdd7dcdabea59c4192201b3a434df21
hmeyd <yyahmedsalm@gmail.com>
2024-12-05 08:56:30 +0100

    modif

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,27 +6,27 @@
 /**
  * print_list - Prints all the elements of a linked list
  * @h: The head of the linked list
- * 
+ *
  * Return: The number of nodes
  */
 size_t print_list(const list_t *h)
 {
-    int n_noeud = 0;
-    const list_t *current = h;
+	int n_noeud = 0;
+	const list_t *current = h;
 
-    while (current != NULL)
-    {
-        n_noeud++;
-        if (current->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%d] %s\n", current->len, current->str);
-        }
-        current = current->next;
-    }
-    return (n_noeud);
+	while (current != NULL)
+	{
+		n_noeud++;
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", current->len, current->str);
+		}
+		current = current->next;
+	}
+	return (n_noeud);
 }
 

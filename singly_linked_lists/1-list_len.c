de6288e6e4c5579e42dba8ecf63d4f50fc927d57
hmeyd <yyahmedsalm@gmail.com>
2024-12-05 08:59:38 +0100

    modif

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -6,19 +6,19 @@
 /**
  * list_len - Returns the number of elements in a linked list
  * @h: The head of the linked list
- * 
+ *
  * Return: The number of nodes
  */
 size_t list_len(const list_t *h)
 {
-    int n_noeud = 0;
-    const list_t *current = h;
+	int n_noeud = 0;
+	const list_t *current = h;
 
-    while (current != NULL)
-    {
-        n_noeud++;
-        current = current->next;
-    }
-    return (n_noeud);
+	while (current != NULL)
+	{
+		n_noeud++;
+		current = current->next;
+	}
+	return (n_noeud);
 }
 

823ea5d7a8b14568f20312014034bd60af090c5f
hmeyd <yyahmedsalm@gmail.com>
2024-12-07 02:21:09 +0100

    permissions

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
 
@@ -24,17 +25,23 @@ int main(int argc, char *argv[])
         exit(97);
     }
 
-    fd_from = open(argv[1], O_RDONLY, 0664);
+    fd_from = open(argv[1], O_RDONLY);
     if (fd_from == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
         exit(98);
     }
 
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
     if (fd_to == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+        exit(99);
+    }
+
+    if (fchmod(fd_to, 0664) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't set permissions for file %s\n", argv[2]);
         exit(99);
     }
 

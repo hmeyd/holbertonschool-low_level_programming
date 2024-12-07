6067cddb361cc880d33c735b3f71a8c1a9b32f52
hmeyd <yyahmedsalm@gmail.com>
2024-12-07 02:04:21 +0100

    modif

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -24,14 +24,14 @@ int main(int argc, char *argv[])
         exit(97);
     }
 
-    fd_from = open(argv[1], O_RDONLY);
+    fd_from = open(argv[1], O_RDONLY, 0664);
     if (fd_from == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
         exit(98);
     }
 
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
     if (fd_to == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
